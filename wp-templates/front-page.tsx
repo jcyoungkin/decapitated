46b885f3fbbec71bb3accd3a9be90824a4aa994b
Clay Youngkin <clay.youngkin@wpengine.com>
2021-02-25 15:34:50 -0600

    removed Posts from main

--- wp-templates/front-page.tsx
+++ wp-templates/front-page.tsx
@@ -21,16 +21,8 @@ export default function FrontPage(props: any): JSX.Element {
             Some great examples of the Headless Horseman include the animated classic from<a href="https://disney.fandom.com/wiki/The_Headless_Horseman">Disney</a>, as well as the Johnny Depp version, <a href="https://en.wikipedia.org/wiki/Sleepy_Hollow_(film)">Sleepy Hollow</a>, but don't forget to read up on the <a href="https://en.wikipedia.org/wiki/The_Legend_of_Sleepy_Hollow">Legen of Sleepy Hollow</a> to get a better understanding of where this fiction originates from.
           </p>
         </Hero>
-
-        <Posts
-          posts={posts?.nodes}
-          headingLevel="h2"
-          postTitleLevel="h3"
-          count={6}
-          id={styles.post_list}
-        />
       </main>
-      
+
     </>
   );
 }
